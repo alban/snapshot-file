e05f7d00d7e2e382e891ec7739a8a90b3446c30f
Alban Crequy <albancrequy@linux.microsoft.com>
2024-05-11 17:07:35 +0200

    Initial code for snapshot-file
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

--- README.md
+++ README.md
@@ -1,35 +1,22 @@
-# gadget-template
+# snapshot-file
 
-This repository is a template repository to help you create your own gadgets.
-
-Steps to use this template:
-- Click on [use this template](https://github.com/new?template_name=gadget-template&template_owner=inspektor-gadget)
-- Choose a name for your repository
-- Click on *Create repository*
-- Update the placeholders (`git grep -i CHANGEME`, `git grep -i TODO`)
-- Write your eBPF program (follow [Hello world gadget](https://inspektor-gadget.io/docs/latest/devel/hello-world-gadget/))
-- Delete this section from README.md
-
----
-
-# CHANGEME-GADGET-NAME
-
-CHANGEME-GADGET-NAME is a [gadget from Inspektor
-Gadget](https://inspektor-gadget.io/). It detects CHANGEME...
+snapshot-file is a [gadget from Inspektor
+Gadget](https://inspektor-gadget.io/). It gather information about open files.
 
 ## How to use
 
 ```bash
 $ export IG_EXPERIMENTAL=true
-$ sudo -E ig run ghcr.io/CHANGEME-ORG/CHANGEME-GADGET-NAME:latest
+$ sudo -E ig run ghcr.io/alban/snapshot-file:latest
 ```
 
 ## Requirements
 
-- ig v0.26.0 (CHANGEME)
-- Linux v5.15 (CHANGEME)
+- ig v0.26.0 (TBD)
+- Linux v6.0
+  - [ksym iterators](https://github.com/torvalds/linux/commit/647cafa223490)
 
-## License (CHANGEME)
+## License
 
 The user space components are licensed under the [Apache License, Version
 2.0](LICENSE). The BPF code templates are licensed under the [General Public
--- program.bpf.c
+++ program.bpf.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
-/* Copyright (c) 2024 CHANGEME-Authors */
+/* Copyright (c) 2024 The Inspektor Gadget authors */
 
 #include <vmlinux.h>
 
@@ -16,34 +16,120 @@
 #define TASK_COMM_LEN 16
 #endif
 
-struct event {
-  gadget_timestamp timestamp;
-  gadget_mntns_id mntns_id;
-  __u32 pid;
-  __u8 comm[TASK_COMM_LEN];
+#ifndef KSYM_NAME_LEN
+#define KSYM_NAME_LEN 512
+#endif
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
+
+struct {
+	const char name[KSYM_NAME_LEN];
+	volatile __u64 addr;
+} addrs[] = {
+	{"socket_file_ops", 0},
+	{"bpf_map_fops_addr", 0},
+	{"bpf_prog_fops_addr", 0},
+	{"bpf_link_fops_addr", 0},
+	{"eventpoll_fops_addr", 0},
+	{"pipe_inode_info_addr", 0},
+	{"tty_fops_addr", 0},
 };
 
-GADGET_TRACER_MAP(events, 1024 * 256);
+struct gadget_file {
+  gadget_mntns_id mntns_id;
+  char comm[TASK_COMM_LEN];
+  __u32 ppid;
+  __u32 pid;
+  __u32 uid;
+  __u32 gid;
+  __u32 fd;
+  __u64 ino;
+  __u64 f_op;
+};
 
-GADGET_TRACER(changeme_mytracer, events, event);
+//GADGET_SNAPSHOTTER(ksyms, gadget_file, iter_ksym);
+GADGET_SNAPSHOTTER(files, gadget_file, iter_file);
 
-SEC("tracepoint/syscalls/sys_enter_chdir")
-int tracepoint__sys_enter_chdir(struct trace_event_raw_sys_enter *ctx) {
-  struct event *event;
-  __u64 pid_tgid = bpf_get_current_pid_tgid();
+// iterators on ksym added in Linux 6.0
 
-  event = gadget_reserve_buf(&events, sizeof(*event));
-  if (!event)
+// For some reason, vmlinux.h only has bpf_iter__ksym in arm64.
+#ifdef __TARGET_ARCH_x86
+struct bpf_iter__ksym {
+        union {
+                struct bpf_iter_meta *meta;
+        };
+        union {
+                struct kallsym_iter *ksym;
+        };
+};
+#endif
+
+/*
+SEC("iter/ksym")
+int iter_ksym(struct bpf_iter__ksym *ctx)
+{
+	struct seq_file *seq = ctx->meta->seq;
+	struct kallsym_iter *iter = ctx->ksym;
+	__u32 seq_num = ctx->meta->seq_num;
+	unsigned long value;
+	char type;
+	int i;
+
+	addrs[0].addr = 42;
+
+	if (!iter)
+		return 0;
+
+#pragma unroll
+	for (i = 0; i < ARRAY_SIZE(addrs); i++) {
+		if (__builtin_memcmp(addrs[i].name, iter->name,
+				     sizeof(addrs[i].name)) == 0)
+			addrs[i].addr = iter->value;
+	}
+	return 0;
+}
+*/
+
+// This iterates on all the open files (from all processes).
+SEC("iter/task_file")
+int iter_file(struct bpf_iter__task_file *ctx) {
+  struct seq_file *seq = ctx->meta->seq;
+  struct file *file = ctx->file;
+  struct task_struct *task = ctx->task;
+  struct task_struct *parent;
+  pid_t parent_pid;
+  u64 mntns_id;
+
+  struct gadget_file gadget_file = {};
+
+  if (!file || !task)
     return 0;
 
-  /* event data */
-  event->timestamp = bpf_ktime_get_boot_ns();
-  event->mntns_id = gadget_get_mntns_id();
-  event->pid = pid_tgid >> 32;
-  bpf_get_current_comm(&event->comm, sizeof(event->comm));
+  mntns_id = (u64)BPF_CORE_READ(task, nsproxy, mnt_ns, ns.inum);
+  if (gadget_should_discard_mntns_id(mntns_id))
+    return 0;
 
-  /* emit event */
-  gadget_submit_buf(ctx, &events, event, sizeof(*event));
+  __u64 f_op = (__u64)(file->f_op);
+
+  parent = task->real_parent;
+  if (!parent)
+    parent_pid = -1;
+  else
+    parent_pid = parent->pid;
+
+  gadget_file.mntns_id = mntns_id;
+  __builtin_memcpy(gadget_file.comm, task->comm, TASK_COMM_LEN);
+  gadget_file.ppid = parent_pid;
+  gadget_file.pid = task->tgid;
+  gadget_file.uid = task->cred->uid.val;
+  gadget_file.gid = task->cred->gid.val;
+  gadget_file.fd = ctx->fd;
+  gadget_file.ino = file->f_inode->i_ino;
+  gadget_file.f_op = f_op;
+
+  bpf_seq_write(seq, &file, sizeof(file));
 
   return 0;
 }
